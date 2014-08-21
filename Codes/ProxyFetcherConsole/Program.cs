0f98472ca8ca39f44763f6144cfa1e803ee874df
yyjt1987 <yyjt1987@126.com>
2014-08-21 19:50:42 +0800

    Add log4net

--- Codes/ProxyFetcherConsole/Program.cs
+++ Codes/ProxyFetcherConsole/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using log4net;
 using ProxyFetcherConsole.Services;
 
 namespace ProxyFetcherConsole
 {
     class Program
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof (Program));
+
         static void Main(string[] args)
         {
+            Log.Error("Test");
             try
             {
                 IProxyFetcherService fetcher = new ProxyFetcherService();
--- Codes/Yujt.ToolBox.Test/UnitTest1.cs
+++ Codes/Yujt.ToolBox.Test/UnitTest1.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using System.Text;
 using NUnit.Framework;
 using Yujt.Common.Helper;
 
